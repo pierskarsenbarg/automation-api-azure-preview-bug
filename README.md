7562054523e43a8aea71b6da7d693ea55cf170da
Piers Karsenbarg <piers@pulumi.com>
2022-12-02 12:20:48 +0000

    Inline program

--- README.md
+++ README.md
@@ -5,12 +5,11 @@ To run:
 
 ## Running Azure classic package v5.17
 
-1. `cd infra && dotnet build`
-1. `cd ../automation && dotnet run`
+1. `cd automation && dotnet run`
 
 Wait for it to complete
 
-1. `dotnet run destroy && cd ..`
+1. `dotnet run destroy`
 
 ## Running Azure classic package v5.25
 
--- automation/Program.cs
+++ automation/Program.cs
@@ -1,35 +1,50 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
+using Pulumi;
 using Pulumi.Automation;
+using Pulumi.Azure.Core;
 
-namespace LocalProgram
+namespace InlineProgram
 {
     class Program
     {
         static async Task Main(string[] args)
         {
+            // define our pulumi program "inline"
+            var program = PulumiFn.Create(() =>
+            {
+                var resourceGroup = new ResourceGroup("resourceGroup");                
+
+                // export the website url
+                return new Dictionary<string, object?>
+                {
+                    ["resourcegroup_name"] = resourceGroup.Name,
+                };
+            });
+
             // to destroy our program, we can run "dotnet run destroy"
             var destroy = args.Any() && args[0] == "destroy";
 
+            var projectName = "inline_azure_project";
             var stackName = "dev";
 
-            // need to account for the assembly executing from within the bin directory
-            // when getting path to the local program
-            var executingDir = new DirectoryInfo(Assembly.GetExecutingAssembly().Location).Parent.FullName;
-            var workingDir = Path.Combine(executingDir, "..", "..", "..", "..", "infra");
-
-            // create our stack using a local program in the ../../../../fargate directory
-            var stackArgs = new LocalProgramArgs(stackName, workingDir);
+            // create or select a stack matching the specified name and project
+            // this will set up a workspace with everything necessary to run our inline program (program)
+            var stackArgs = new InlineProgramArgs(projectName, stackName, program);
             var stack = await LocalWorkspace.CreateOrSelectStackAsync(stackArgs);
 
             Console.WriteLine("successfully initialized stack");
 
+            // for inline programs, we must manage plugins ourselves
+            Console.WriteLine("installing plugins...");
+            await stack.Workspace.InstallPluginAsync("azure", "v5.25.0");
+            Console.WriteLine("plugins installed");
+
             // set stack configuration specifying the region to deploy
             Console.WriteLine("setting up config...");
-            await stack.SetConfigAsync("aws:region", new ConfigValue("us-west-2"));
+            await stack.SetConfigAsync("azure:location", new ConfigValue("uksouth"));
             Console.WriteLine("config set");
 
             Console.WriteLine("refreshing stack...");
@@ -54,7 +69,7 @@ namespace LocalProgram
                         Console.WriteLine($"    {change.Key}: {change.Value}");
                 }
 
-                //Console.WriteLine($"website url: {result.Outputs["WebsiteUrl"].Value}");
+                Console.WriteLine($"resource group name: {result.Outputs["resourcegroup_name"].Value}");
             }
         }
     }
